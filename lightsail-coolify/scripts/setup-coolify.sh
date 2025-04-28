ffc38f361075f89fd1944b4e2fa51ca099c3ea8b
Anton Vusatyi <anton_vusatyi@pg.com>
2025-04-29 00:39:47 +0300

    Updated Readme and setup scripts

--- lightsail-coolify/scripts/setup-coolify.sh
+++ lightsail-coolify/scripts/setup-coolify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update -y
-apt-get install -y docker.io docker-compose curl
+# Installing docker-compose-v2 - https://github.com/coollabsio/coolify/issues/3026#issuecomment-2395297125
+apt-get install -y docker.io docker-compose docker-compose-v2 curl
 systemctl enable docker
 systemctl start docker
 curl -fsSL https://cdn.coollabs.io/coolify/install.sh | bash
